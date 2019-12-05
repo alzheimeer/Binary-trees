18ffeb3d9b0c72987b7779107f24b15ffeffe17d
alzheimeer <fogniebla@hotmail.com>
2019-12-05 10:22:45 -0500

    fix 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,18 +9,19 @@
  **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	int a, b, c;
+	binary_tree_t *a, *b, *c;
 
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	a = node->parent->n;
-	b = node->parent->parent->left->n;
-	c = node->parent->parent->right->n;
+	a = node->parent;
+	b = node->parent->parent->left;
+	c = node->parent->parent->right;
 
-	if (b == a)
-		return (node->parent->parent->right);
-	if (c == a)
-		return (node->parent->parent->left);
+	if (a == b)
+		return (c);
+	else if (a == c)
+		return (b);
+
 	return (NULL);
 }
