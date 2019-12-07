4762ee35228048e4ff8ded509113a6213c75757a
alzheimeer <fogniebla@hotmail.com>
2019-12-06 19:17:38 -0500

    features rotate right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: return a pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *aux;
+
+	if (tree == NULL || tree->left == NULL)
+		return (tree);
+
+	aux = tree->left;
+	aux->parent = tree->parent;
+	tree->parent = aux;
+	tree->left = aux->right;
+	aux->right = tree;
+	return (aux);
+}
