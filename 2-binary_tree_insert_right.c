aec7252fc915016f73cd37233d8f9201d2dbbde0
alzheimeer <fogniebla@hotmail.com>
2020-11-23 21:04:14 -0800

    fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,7 +18,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
-		parent->right = newNode;
 		newNode->right->parent = newNode;
 	}
 	parent->right = newNode;
